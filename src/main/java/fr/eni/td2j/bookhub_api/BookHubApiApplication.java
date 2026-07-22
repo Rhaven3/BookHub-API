f8f4628a631e362f65a701303e66039e2f93d562
FONTAINE Théo <f.nom.utilisateur@gmail.com>
2026-07-22 14:03:46 +0200

    init

---
+++ src/main/java/fr/eni/td2j/bookhub_api/BookHubApiApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.td2j.bookhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/td2j/bookhub_api/BookHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.td2j.bookhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

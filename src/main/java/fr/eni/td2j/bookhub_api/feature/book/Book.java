6780e567d88f317dc60eaa586ff2dd05774a8c6e
io2944 <dumdia79@gmail.com>
2026-07-24 12:06:00 +0200

    feat: enhance Book entity with relationships to Author, Category, Editor, and Image

--- src/main/java/fr/eni/td2j/bookhub_api/feature/book/Book.java
+++ src/main/java/fr/eni/td2j/bookhub_api/feature/book/Book.java
@@ -1,10 +1,16 @@
 package fr.eni.td2j.bookhub_api.feature.book;
 
 import fr.eni.td2j.bookhub_api.common.BaseEntity;
+import fr.eni.td2j.bookhub_api.feature.author.Author;
+import fr.eni.td2j.bookhub_api.feature.category.Category;
+import fr.eni.td2j.bookhub_api.feature.editor.Editor;
+import fr.eni.td2j.bookhub_api.feature.image.Image;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Setter
@@ -36,4 +42,29 @@ public class Book extends BaseEntity {
 
     @Column(nullable = false)
     private boolean owned;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private List<Author> authors = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_category",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "editor_id", nullable = false)
+    private Editor editor;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "book_id")
+    private List<Image> images = new ArrayList<>();
+
 }

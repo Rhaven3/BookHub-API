6c12b399340403da2bd99f68380595fc1b982034
FONTAINE Théo <f.nom.utilisateur@gmail.com>
2026-07-30 16:56:26 +0200

    adapt for method PATCH et apiResponse

--- src/main/java/fr/eni/td2j/bookhub_api/security/SecurityConfig.java
+++ src/main/java/fr/eni/td2j/bookhub_api/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 
         // TODO : remplace par l'URL de TON frontend BookHub
         config.setAllowedOrigins(List.of("http://localhost:4200"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // nécessaire si tu envoies des cookies/credentials
 
